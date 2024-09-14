82d4e2e68b20b80d3f27697c1043402dd42d5056
Enrique Alvarado <alvaradoenrique257@gmail.com>
2024-09-14 00:00:22 -0600

    Mejora de estilos SearchBar 👽🔵

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,11 +1,11 @@
 import './index.css'
-import iconSearch from '../../assets/img/search-icon.svg'
+import iconSearch from '../../assets/img/search-icon2.svg'
 
 const SearchBar = () => {
   return (
     <div className='input-container'>
         <div className='icon-container'>
-            <img className='icon-search' src= {iconSearch} alt="search icon" />
+            <img className='icon-search' src= {iconSearch} placeholder="search icon" />
         </div>
         <input className='input-search' type="text" placeholder='Search books ...' />
     </div>
